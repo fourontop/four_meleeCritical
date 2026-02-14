86e627180516b4d2ac58b33d806f86fd3359433c
Four <114292038+fourontop@users.noreply.github.com>
2026-02-14 20:15:49 +0700

    Update fxmanifest.lua

--- four_meleeCritical/fxmanifest.lua
+++ four_meleeCritical/fxmanifest.lua
@@ -1,5 +1,3 @@
--- NC PROTECT+
-client_scripts { '@nc_PROTECT+/exports/protected.lua', '@nc_PROTECT+/exports/cl.lua' }
 
 fx_version 'adamant' 
 game 'gta5' 
@@ -7,6 +5,7 @@ game 'gta5'
 client_scripts {
     "config.lua",
     "client/client.lua"
+
 }
 
 lua54 'yes'
